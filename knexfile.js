b610abd21a8e49166218a3895b23ae299b97f460
luanhsd <luan_h.s.d@hotmail.com>
2020-05-25 00:07:17 -0300

    start backend

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: process.env.CLIENT,
+    connection: {
+      host: process.env.HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    }
+  },
+  /*
+    staging: {
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user: 'username',
+        password: 'password'
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
+    },
+  
+    production: {
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user: 'username',
+        password: 'password'
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
+    }
+  */
+};
